2bc6a45fc1bd2955d06d18778d49706277b49da3
mackenzie <evans.mackenzie@gmail.com>
2021-03-11 18:36:18 -0500

    works on making graph for variables index page. not finished

--- src/views/VariableIndex.vue
+++ src/views/VariableIndex.vue
@@ -7,6 +7,9 @@
         <p>value: {{ entry.value }}</p>
         <p>created: {{ relativeDate(entry.created_at) }}</p>
       </div>
+      <div>
+        <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
+      </div>
       <router-link :to="`variables/${variable.id}`">More Info</router-link>
     </div>
   </div>
@@ -15,17 +18,74 @@
 <script>
 import axios from "axios";
 import moment from "moment";
+import { Chart } from "highcharts-vue";
 
 export default {
+  components: {
+    highcharts: Chart,
+  },
   data: function() {
     return {
       variables: [],
+      chartOptions: {
+        title: {
+          text: "Monthly Average Temperature",
+        },
+        subtitle: {
+          text: "Source: WorldClimate.com",
+        },
+        xAxis: {
+          categories: [
+            "Mon",
+            "Tues",
+            "Wed",
+            "Thurs",
+            "Fri",
+            "Jun",
+            "Jul",
+            "Aug",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec",
+          ],
+        },
+        yAxis: {
+          title: {
+            text: "Entry Value",
+          },
+        },
+        plotOptions: {
+          line: {
+            dataLabels: {
+              enabled: true,
+            },
+            enableMouseTracking: false,
+          },
+        },
+        chart: {
+          type: "line",
+        },
+        series: [
+          {
+            data: [1, 2, 3],
+          },
+        ],
+      },
     };
   },
   created: function() {
     axios.get(`/api/variables`).then((response) => {
       console.log(response.data);
       this.variables = response.data;
+      this.variables.map((variable) =>
+        this.chartOptions.series.push({ data: variable.entries })
+      );
+      this.chartOptions.series.shift();
+      console.log(this.chartOptions.series);
+      // this.chartOptions.series[0].data = this.variables[0].entries.map(
+      //   (entry) => entry.value
+      // );
     });
   },
   methods: {
