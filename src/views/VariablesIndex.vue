cb5babc5f53bc685ffa07a654ed605e0468713c2
mackenzie <evans.mackenzie@gmail.com>
2021-03-19 14:49:51 -0400

    fixes variables index page to make graph filterable by date

--- src/views/VariablesIndex.vue
+++ src/views/VariablesIndex.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="variables-index">
     <h1>Variable Graph</h1>
-    <p>{{ relativeTodayDate(today) }}</p>
+    <p>{{ formatDate(today) }}</p>
     <div>
       <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
     </div>
 
-    <form>
-      <div class="form-group">
-        <label>Date Search:</label>
-        <input type="text" class="form-control" v-model="date" />
-      </div>
-      <button v-on:click="searchDate(date)">Search</button>
-      <!-- <input type="submit" class="btn btn-primary" value="Submit" /> -->
-    </form>
+    <div>
+      <label>Date Search:</label>
+      <input type="text" class="form-control" v-model="newFinalDate" />
+    </div>
+    <button v-on:click="generateXAxisCategories(newFinalDate)">Search</button>
 
     <div v-for="variable in variables" v-bind:key="variable.id">
       <h2>{{ variable.name }}</h2>
@@ -42,7 +39,7 @@ export default {
       today: "",
       dateRange: [],
       newDateRange: [],
-      date: null,
+      newFinalDate: "",
       chartOptions: {
         title: {
           text: "Variable Stuff",
@@ -79,91 +76,87 @@ export default {
       console.log(response.data);
       this.variables = response.data;
     });
-    this.today = new Date();
-    var dd = String(this.today.getDate()).padStart(2, "0");
-    var mm = String(this.today.getMonth() + 1).padStart(2, "0"); //January is 0!
-    var yyyy = this.today.getFullYear();
-    this.today = mm + "/" + dd + "/" + yyyy;
-    console.log(this.today);
-
-    console.log(
-      this.relativeTodayDate(new Date(this.relativeDate(this.today)))
-    );
-
-    var startDate = new Date(this.relativeDate(this.today));
-    for (var i = 2; i < 19; i++) {
-      startDate.setDate(i);
-      var date = startDate.toDateString();
-      this.dateRange.push(this.relativeTodayDate(date));
-    }
-
-    // console.log(this.dateRange);
+    this.findTodaysDate();
   },
   methods: {
-    relativeDate: function(date) {
+    findTodaysDate: function() {
+      this.today = new Date();
+      var dd = String(this.today.getDate()).padStart(2, "0");
+      var mm = String(this.today.getMonth() + 1).padStart(2, "0"); //January is 0!
+      var yyyy = this.today.getFullYear();
+      this.today = mm + "/" + dd + "/" + yyyy;
+      console.log("Today's date:", this.today);
+    },
+    generateXAxisCategories: function(finalDate) {
+      this.dateRange = [];
+      // finds start date based on final date given (2 weeks past)
+      var startDate = new Date(this.beginningDate(finalDate));
+      console.log("New start date:", startDate);
+      // uses start date to genereate new date range
+      for (var i = 0; i < 15; i++) {
+        var date = moment(startDate).add(i, "days");
+        console.log("In loop, date changing", date);
+        this.dateRange.push(this.formatDate(date));
+      }
+      this.chartOptions.xAxis.categories = this.dateRange;
+      console.log(this.dateRange);
+      // if there are any variables currently plotted on the chart (if theres series data), then call the plotVariable method on each one
+      if (this.chartOptions.series != []) {
+        var variableNames = [];
+        this.chartOptions.series.forEach(function(serie) {
+          variableNames.push(serie.name);
+        });
+        this.chartOptions.series.length = 0;
+        console.log(variableNames);
+        this.variables.forEach(
+          function(variable) {
+            if (variableNames.includes(variable.name)) {
+              this.plotVariable(variable);
+            }
+          }.bind(this)
+        );
+      }
+    },
+    beginningDate: function(date) {
       return moment(date)
-        .subtract(13, "days")
+        .subtract(14, "days")
         .calendar();
     },
-    relativeTodayDate: function(date) {
+    formatDate: function(date) {
       return moment(date).format("MM/DD/YYYY");
     },
     plotVariable: function(variable) {
-      var newSeries = {};
-      newSeries.name = variable.name;
-      // newSeries.data = variable.entries.map((entry) => entry.rating);
-      // if (this.chartOptions.series.length < 1) {
-      // this.chartOptions.xAxis.categories = variable.entries.map(
-      //   (entry) => entry.created_at
-      // );
-      // }
-      var graphView = {};
-      this.dateRange.forEach(function(createDate) {
-        graphView[createDate] = null;
-      });
-      // var ratings = [];
-      // this.dateRange.forEach((date) => {
-      //   variable.entries.forEach(function(entry) {
-      //     if (date == entry.created_at) {
-      //       ratings.push(entry.rating);
-      //     } else {
-      //       ratings.push(null);
-      //     }
-      //   });
-      // });
-      // console.log(ratings);
-      // var ratings = []
-      var dates = this.dateRange;
-      console.log(dates);
-      variable.entries.forEach(function(entry) {
-        if (dates.includes(entry.created_at)) {
-          graphView[entry.created_at] = entry.rating;
-        }
+      if (this.chartOptions.xAxis.categories == 0) {
+        this.generateXAxisCategories(this.today);
+      }
+
+      var xAxisDateRatings = {};
+      this.dateRange.forEach(function(xAxisDate) {
+        xAxisDateRatings[xAxisDate] = null;
       });
 
-      console.log(graphView);
+      variable.entries.forEach(
+        function(entry) {
+          if (this.dateRange.includes(entry.created_at)) {
+            xAxisDateRatings[entry.created_at] = entry.rating;
+          }
+        }.bind(this)
+      );
+
+      console.log("xAxisDateRatings object:", xAxisDateRatings);
+
       var newSeriesData = [];
 
-      for (const prop in graphView) {
-        newSeriesData.push(parseInt(`${graphView[prop]}`));
+      for (const rating in xAxisDateRatings) {
+        newSeriesData.push(xAxisDateRatings[rating]);
       }
-      console.log(newSeriesData);
+      console.log("New Series Data:", newSeriesData);
+
+      var newSeries = {};
+      newSeries.name = variable.name;
       newSeries.data = newSeriesData;
 
-      this.chartOptions.xAxis.categories = this.dateRange;
-
       this.chartOptions.series.push(newSeries);
-    },
-    searchDate: function(date) {
-      var newStartDate = new Date(this.relativeDate(date));
-
-      for (var i = 2; i < 19; i++) {
-        newStartDate.setDate(i);
-        this.newDateRange.push(newStartDate.toDateString());
-      }
-
-      console.log(this.newDateRange);
-      this.chartOptions.xAxis.categories = this.newDateRange;
     },
   },
 };
