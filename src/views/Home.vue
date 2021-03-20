2319c8dda16a11990f5c660a7de1ed912f79c89d
mackenzie <evans.mackenzie@gmail.com>
2021-03-20 14:39:48 -0400

    installs unify bootstrap theme. Styles signup and login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,144 @@
+/* eslint-disable vue/no-parsing-error */
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <form v-on:submit.prevent="submit()">
-      <h2>Signup</h2>
-      <router-link to="/login">Login</router-link>
-      <ul>
-        <li class="text-danger" v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </li>
-      </ul>
-      <div class="form-group">
-        <label>Name:</label>
-        <input type="text" class="form-control" v-model="name" />
+    <!-- Signup -->
+    <section class="g-bg-gray-light-v5">
+      <h1>{{ message }}</h1>
+      <div class="container g-py-100">
+        <div class="row justify-content-center">
+          <div class="col-sm-10 col-md-9 col-lg-6">
+            <div class="u-shadow-v21 g-bg-white rounded g-py-40 g-px-30">
+              <header class="text-center mb-4">
+                <h2 class="h2 g-color-black g-font-weight-600">Signup</h2>
+              </header>
+
+              <!-- Form -->
+              <form class="g-py-15" v-on:submit.prevent="submit()">
+                <ul>
+                  <li
+                    class="text-danger"
+                    v-for="error in errors"
+                    v-bind:key="error"
+                  >
+                    {{ error }}
+                  </li>
+                </ul>
+                <div class="mb-4">
+                  <label
+                    class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13"
+                    >Name:</label
+                  >
+                  <input
+                    class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15"
+                    type="text"
+                    placeholder="John"
+                    v-model="name"
+                  />
+                </div>
+                <!-- my code -->
+                <!-- <div class="form-group">
+                  <label>Name:</label>
+                  <input type="text" class="form-control" v-model="name" />
+                </div> -->
+
+                <div class="mb-4">
+                  <label
+                    class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13"
+                    >Email:</label
+                  >
+                  <input
+                    class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15"
+                    type="email"
+                    placeholder="johndoe@gmail.com"
+                    v-model="email"
+                  />
+                </div>
+                <!-- my code -->
+                <!-- <div class="form-group">
+                  <label>Email:</label>
+                  <input type="email" class="form-control" v-model="email" />
+                </div> -->
+
+                <div class="row">
+                  <div class="col-xs-12 col-sm-6 mb-4">
+                    <label
+                      class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13"
+                      >Password:</label
+                    >
+                    <input
+                      class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15"
+                      type="password"
+                      placeholder="Password"
+                      v-model="password"
+                    />
+                  </div>
+                  <!-- my code -->
+                  <!-- <div class="form-group">
+                    <label>Password:</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      v-model="password"
+                    />
+                  </div> -->
+
+                  <div class="col-xs-12 col-sm-6 mb-4">
+                    <label
+                      class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13"
+                      >Confirm Password:</label
+                    >
+                    <input
+                      class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15"
+                      type="password"
+                      placeholder="Password"
+                      v-model="passwordConfirmation"
+                    />
+                  </div>
+                  <!-- my code -->
+                  <!-- <div class="form-group">
+                    <label>Password confirmation:</label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      v-model="passwordConfirmation"
+                    />
+                  </div> -->
+                </div>
+                <div class="mb-4">
+                  <input type="submit" class="btn btn-primary" value="Signup" />
+                </div>
+              </form>
+              <!-- End Form -->
+
+              <footer class="text-center">
+                <p class="g-color-gray-dark-v5 g-font-size-13 mb-0">
+                  Already have an account?
+                  <router-link class="g-font-weight-600" to="/login"
+                    >Login</router-link
+                  >
+                </p>
+              </footer>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
-      </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" />
-      </div>
-      <div class="form-group">
-        <label>Password confirmation:</label>
-        <input
-          type="password"
-          class="form-control"
-          v-model="passwordConfirmation"
-        />
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
+    </section>
+    <!-- End Signup -->
+
+    <a
+      class="js-go-to u-go-to-v1"
+      href="#"
+      data-type="fixed"
+      data-position='{
+     "bottom": 15,
+     "right": 15
+   }'
+      data-offset-top="400"
+      data-compensation="#js-header"
+      data-show-effect="zoomIn"
+    >
+      <i class="hs-icon hs-icon-arrow-top"></i>
+    </a>
   </div>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,22 +1,92 @@
 <template>
   <div class="login">
-    <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-        <li class="text-danger" v-for="error in errors" v-bind:key="error">
-          {{ error }}
-        </li>
-      </ul>
-      <div class="form-group">
-        <label>Email:</label>
-        <input type="email" class="form-control" v-model="email" />
+    <!-- Login -->
+    <section class="g-bg-gray-light-v5">
+      <div class="container g-py-100">
+        <div class="row justify-content-center">
+          <div class="col-sm-8 col-lg-5">
+            <div class="u-shadow-v21 g-bg-white rounded g-py-40 g-px-30">
+              <header class="text-center mb-4">
+                <h2 class="h2 g-color-black g-font-weight-600">Login</h2>
+              </header>
+
+              <!-- Form -->
+              <form class="g-py-15" v-on:submit.prevent="submit()">
+                <ul>
+                  <li
+                    class="text-danger"
+                    v-for="error in errors"
+                    v-bind:key="error"
+                  >
+                    {{ error }}
+                  </li>
+                </ul>
+                <div class="mb-4">
+                  <label
+                    class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13"
+                    >Email:</label
+                  >
+                  <input
+                    class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15"
+                    type="email"
+                    placeholder="johndoe@gmail.com"
+                    v-model="email"
+                  />
+                </div>
+                <!-- my code -->
+                <!-- <div class="form-group">
+                  <label>Email:</label>
+                  <input type="email" class="form-control" v-model="email" />
+                </div> -->
+
+                <div class="g-mb-35">
+                  <label
+                    class="g-color-gray-dark-v2 g-font-weight-600 g-font-size-13"
+                    >Password:</label
+                  >
+                  <input
+                    class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15 mb-3"
+                    type="password"
+                    placeholder="Password"
+                    v-model="password"
+                  />
+                </div>
+
+                <div class="mb-4">
+                  <input type="submit" class="btn btn-primary" value="Login" />
+                </div>
+              </form>
+              <!-- End Form -->
+
+              <footer class="text-center">
+                <p class="g-color-gray-dark-v5 g-font-size-13 mb-0">
+                  Don't have an account?
+                  <router-link class="g-font-weight-600" to="/"
+                    >signup</router-link
+                  >
+                </p>
+              </footer>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="form-group">
-        <label>Password:</label>
-        <input type="password" class="form-control" v-model="password" />
-      </div>
-      <input type="submit" class="btn btn-primary" value="Submit" />
-    </form>
+    </section>
+    <!-- End Login -->
+
+    <a
+      class="js-go-to u-go-to-v1"
+      href="#"
+      data-type="fixed"
+      data-position='{
+     "bottom": 15,
+     "right": 15
+   }'
+      data-offset-top="400"
+      data-compensation="#js-header"
+      data-show-effect="zoomIn"
+    >
+      <i class="hs-icon hs-icon-arrow-top"></i>
+    </a>
   </div>
 </template>
 
